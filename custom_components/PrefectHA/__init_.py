71bc86c9ddad0b2dd2f83da6b676fcf19f5db90f
Jason Sievert <jsievert@gmail.com>
2019-08-30 22:16:18 -0500

    Adding skeleton init

---
+++ custom_components/PrefectHA/__init_.py
@@ -0,0 +1,48 @@
+"""Support for My Ford Mobile vehicles."""
+import logging
+from datetime import timedelta
+
+import voluptuous as vol
+
+from homeassistant.components.lock import DOMAIN as LOCK
+from homeassistant.components.sensor import DOMAIN as SENSOR
+from homeassistant.components.switch import DOMAIN as SWITCH
+from homeassistant.const import (
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_SCAN_INTERVAL
+)
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.discovery import load_platform
+from homeassistant.helpers.dispatcher import dispatcher_send
+from homeassistant.helpers.event import track_time_interval
+
+DOMAIN = "prefect"
+DATA_UPDATED = "{}_data_updated".format(DOMAIN)
+
+_LOGGER = logging.getLogger(__name__)
+
+SUPPORTED_PLATFORMS = [LOCK, SENSOR, SWITCH]
+
+DEFAULT_INTERVAL = timedelta(days=7)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+                vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_INTERVAL): vol.All(
+                    cv.time_period, cv.positive_timedelta
+                ),
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+def setup(hass, config):
+    """Set up the Prefect component."""
+    import prefect
+
+    conf = config[DOMAIN]
